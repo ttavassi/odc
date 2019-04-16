5c90c7555d42b719278a01df602da681dde66a0d
ttavassi <49685049+ttavassi@users.noreply.github.com>
2019-04-16 16:00:44 +0200

    Add files via upload

---
+++ Info.swift
@@ -0,0 +1,30 @@
+//
+//  Info.swift
+//  Ondes de Choq
+//
+//  Created by Tomaz Tavassi on 16/04/19.
+//  Copyright © 2019 Tomaz Tavassi. All rights reserved.
+//
+
+import UIKit
+
+class Info: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UserViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UserViewController.swift
+//  Ondes de Choq
+//
+//  Created by Tomaz Tavassi on 16/04/19.
+//  Copyright © 2019 Tomaz Tavassi. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    @IBOutlet weak var StVenant: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
